2b90e5059014950eb1c35c699fb0e5db1587f6c2
Alice Parrish <alice@pop-os.localdomain>
2023-04-26 01:01:34 -0400

    Added stop button

--- src/Components/PlaybackControls.tsx
+++ src/Components/PlaybackControls.tsx
@@ -24,18 +24,48 @@ function PlaybackControls() {
             for (let i = 0; i < grid.length; i++) {
                 for (let j = 0; j < grid[i].length; j++) {
                     if (grid[i][j]) {
-                        synths[i].triggerAttackRelease(NOTES[i], "8n", `+0:${j/2}`)
+                        synths[i].triggerAttackRelease(NOTES[i], "8n", j > 0 ? `+0:${j/2}` : time);
                     }
                 }
             }
 
         }, `${numMeasures}m`).start(0));
 
-        Tone.Transport.start();
+        Tone.Transport.start(Tone.now());
     }
 
+    const stop = () => {
+        Tone.Transport.cancel(0);
+        Tone.Transport.stop(0);
+        loop.current.stop(0);
+        synths.forEach(synth => {
+            synth.oscillator.stop();
+        })
+    }
+
+    // const play = () => {
+    //     for (let i = 0; i < grid.length; i++) {
+    //         for (let j = 0; j < grid[i].length; j++) {
+    //             if (grid[i][j]) {
+    //                 const loop = new Tone.Loop(time => {
+    //                     synths[i].triggerAttackRelease(NOTES[i], "8n", time);
+    //                 }, `${numMeasures}m`).start(`+0:${j/2}`)
+    //             }
+    //         }
+    //     }
+
+    //     Tone.Transport.start(Tone.now());
+    // }
+
+    // const stop = () => {
+    //     Tone.Transport.stop(Tone.now());
+    // }
+
     return (
-        <button onClick={play}>Play</button>
+        <>
+            <button onClick={play}>Play</button>
+            <button onClick={stop}>Stop</button>
+        </>
     )
 
 }
